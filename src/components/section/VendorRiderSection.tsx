e0b1bd451ba02e0f57c16f47eb73758c187d49de
siradji <siradjiawoual@gmail.com>
2022-12-18 06:23:40 +0100

    Vendors riders

--- src/components/section/VendorRiderSection.tsx
+++ src/components/section/VendorRiderSection.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import RiderImage from '@assets/png/rider.png'
+import VendorImage from "@assets/svg/vendor.svg"
+
 export function VendorRiderSection (): JSX.Element {
     return (
         <section id='#vendorsRiders' className='my-20'>
             <div className='flex flex-col md:flex-row items-center justify-between'>
                 <div className='bg-[#798293] flex flex-col items-center w-full  md:w-1/2 px-10 md:px-28 lg:px-40  py-10 md:py-20 lg:py-28'>
-                    <Image src={RiderImage} alt='EatLater Rider' className='w-[156px] lg:w-[300px] h-[128px] lg:h-[250px]'/>
+                    <Image src={VendorImage} alt='EatLater Rider' className='w-[156px] lg:w-[300px] h-[128px] lg:h-[250px]'/>
                     <div className='text-white mt-10'>
                         <h1 className='text-3xl text-center font-bold mb-3'>Become a vendor</h1>
                         <p className='font-light text-lg leading-7'>Drive sales by reaching out to wider audience. Sell on EatLater and boost your sales, grow your business and more </p>
